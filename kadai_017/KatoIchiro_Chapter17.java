59cccdded68bd727e854c9ecaa46ef8b3f1b5940
GitHub-0a <148424822+GitHub-0a@users.noreply.github.com>
2023-11-21 20:26:19 +0900

    Add files via upload

--- kadai_017/KatoIchiro_Chapter17.java
+++ kadai_017/KatoIchiro_Chapter17.java
@@ -2,24 +2,26 @@ package kadai_017;
 
 public class KatoIchiro_Chapter17 extends Kato_Chapter17 {
 
-	   public void commonIntroduce(String familyName, String address){
-		   System.out.println("住所は"+address+"です");
-	   }
-	   
-	   public void setGivenName(String givenName) {
-		   givenName = "一郎";
-		   System.out.println("名前は"+familyName+givenName+"です");
-	   }
-	   
-	   //個別紹介出力を実行
+	   //個別紹介を実行する
 	   public void execIntroduce() {
+		   setGivenName();
+		   commonIntroduce();
 		   eachIntroduce();
 	   }
 	   
-	   //個別紹介出力　抽象メソッド 
+	   public void setGivenName() {
+		   givenName = "一郎";
+	   }
+	   
+	   //個別の紹介を出力する　抽象メソッド 
 	   public void eachIntroduce() {
 		   System.out.println("好きな食べ物はリンゴです");
 		   System.out.println("");
 	   }
-
+	   
+	   public void commonIntroduce(){
+		   System.out.println("名前は加藤"+givenName+"です");
+		   System.out.println("住所は"+address+"です");
+	   }
+	
 }
--- kadai_017/Kato_Chapter17.java
+++ kadai_017/Kato_Chapter17.java
@@ -1,14 +1,22 @@
 package kadai_017;
 
 abstract public class Kato_Chapter17 {
-	   //姓フィールド
-	   String familyName = "加藤";
-	   //名フィールド
-	   String givenName;
-	   //住所フィールド
-	   String address = "東京都中野区〇×";
+	//姓フィールド
+	public String familyName = "加藤";
+	//名フィールド
+	public String givenName;
+	//住所フィールド
+	public String address = "東京都中野区〇×";
+	
+	// 個別紹介出力　親クラス抽象メソッド
+	abstract public void eachIntroduce();
+	
+	//紹介を実行する
+	public void execIntroduce(String familyName, String address){
+	}
 	   
-	   // 個別紹介出力　親クラス抽象メソッド
-	   abstract public void eachIntroduce();
-
+	//共通の紹介を出力する
+	public void commonIntroduce(String familyName, String address){
+	}
 }
+  
