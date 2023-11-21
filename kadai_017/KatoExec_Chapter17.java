7bd76f2f7b3745a03c5c354cece025292e970426
GitHub-0a <148424822+GitHub-0a@users.noreply.github.com>
2023-11-21 20:29:32 +0900

    Add files via upload

--- kadai_017/KatoExec_Chapter17.java
+++ kadai_017/KatoExec_Chapter17.java
@@ -3,7 +3,7 @@ package kadai_017;
 public class KatoExec_Chapter17 {
 
 	public static void main(String[] args) {
-		//final String givenName;
+		
 		//オブジェクトを作る
 		KatoTaro_Chapter17   katoTaro   = new KatoTaro_Chapter17();
 		KatoIchiro_Chapter17 katoIchiro = new KatoIchiro_Chapter17();
