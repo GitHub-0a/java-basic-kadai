a085e5ebda62a66dbd898934d26dc430a241ded0
GitHub-0a <148424822+GitHub-0a@users.noreply.github.com>
2023-11-23 16:08:13 +0900

    Add files via upload

--- kadai_020/Dictionary_Chapter20.java
+++ kadai_020/Dictionary_Chapter20.java
@@ -7,27 +7,27 @@ public class Dictionary_Chapter20 {
 	public void Dictionary_Chapter20(String innlist[]) {
 		
 		HashMap<String,String> fMap = new HashMap<String,String>();
-	    fMap.put("apple1","りんご");
-	    fMap.put("peach","桃");
-	    fMap.put("banana","バナナ");
-	    fMap.put("lemon","レモン");
-	    fMap.put("pear","梨");
-	    fMap.put("kiwi","キウィ");
-	    fMap.put("strawberry","いちご");
-	    fMap.put("grape","ぶどう");
-	    fMap.put("muscat","マスカット");
-	    fMap.put("cherry","さくらんぼ");
+		fMap.put("apple1","りんご");
+		fMap.put("peach","桃");
+		fMap.put("banana","バナナ");
+		fMap.put("lemon","レモン");
+		fMap.put("pear","梨");
+		fMap.put("kiwi","キウィ");
+		fMap.put("strawberry","いちご");
+		fMap.put("grape","ぶどう");
+		fMap.put("muscat","マスカット");
+		fMap.put("cherry","さくらんぼ");
 	    
-	     for(int i = 0; i < innlist.length; i++) {
-               String getlist = innlist[i];
-               String getmap = fMap.get(getlist);
-               if (getmap != null) {
-	               System.out.println(getlist+"の意味は" + getmap);
-               }
-               else { 
-                   System.out.println(getlist+"は辞書に存在しません");
-               }
-	     }
+		for(int i = 0; i < innlist.length; i++) {
+			String getlist = innlist[i];
+			String getmap = fMap.get(getlist);
+			 if (getmap != null) {
+				 System.out.println(getlist+"の意味は" + getmap);
+			 }
+			else { 
+				System.out.println(getlist+"は辞書に存在しません");
+			}
+		}
 		
 	}
 
