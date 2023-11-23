b28a0b92b4bd93ab9aeb945f8a7ae378b3c6d217
GitHub-0a <148424822+GitHub-0a@users.noreply.github.com>
2023-11-24 02:24:28 +0900

    Add files via upload

--- kadai_024/Jyanken_Chapter24.java
+++ kadai_024/Jyanken_Chapter24.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 
 public class Jyanken_Chapter24 {
 
-	public String  getMyChoice() {;
+	public String  getMyChoice() {
 		System.out.println("自分のじゃんけんの手を入力しましょう");
 		System.out.println("グーはrockのrを入力しましょう");
 		System.out.println("チョキはscissorsのsを入力しましょう");
 		System.out.println("パーはpaperのpを入力しましょう");
 		
-    	Scanner scanner = new Scanner(System.in);  //Scannerクラスのオブジェクトを生成する
-    	String input= scanner.next();	//入力を取得する
-    	scanner.close();
+		Scanner scanner = new Scanner(System.in);  //Scannerクラスのオブジェクトを生成する
+		String input= scanner.next();	//入力を取得する
+		scanner.close();
 		return input;
 	}
 	
@@ -32,8 +32,8 @@ public class Jyanken_Chapter24 {
 		jMap.put("s","チョキ");
 		jMap.put("p","パー");
 		
-        String getmap_me = jMap.get(aaa);
-        String getmap_you = jMap.get(bbb);
+		String getmap_me = jMap.get(aaa);
+		String getmap_you = jMap.get(bbb);
 		System.out.println("自分の手は"+getmap_me+"、対戦相手の手は"+getmap_you);
 		
 		boolean ccc = aaa.equals(bbb);
@@ -41,8 +41,8 @@ public class Jyanken_Chapter24 {
 			System.out.println("あいこです");
 		}
 		else if ((getmap_me == "グー" && getmap_you == "チョキ") ||
-				 (getmap_me == "チョキ" && getmap_you == "パー") ||
-				 (getmap_me == "パー" && getmap_you =="グー")) {
+			(getmap_me == "チョキ" && getmap_you == "パー") ||
+			(getmap_me == "パー" && getmap_you =="グー")) {
 			System.out.println("自分の勝ちです");
 		}
 		else {
